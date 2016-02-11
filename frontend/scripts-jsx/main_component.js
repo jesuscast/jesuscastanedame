a9cfd1fc60a3631ee4d329dc68f26e9d9b0304c4
jesuscast <jesus.cast.sosa@gmail.com>
2016-02-10 18:06:34 -0800

    Starting to take shape

---
+++ frontend/scripts-jsx/main_component.js
@@ -0,0 +1,30 @@
+// 
+// App wrapper.
+//
+// Just to make it look nice. In fact everything could be in the main container.
+// But this is a better organization.
+//
+// var App = React.createClass({
+//     getInitialState: function(){
+//         return {
+//             screen: 'index'
+//         }
+//     },
+//     handleClick: function(requested_screen) {
+//         this.setState({
+//             screen: requested_screen
+//         });
+//     },
+//     render: function(){
+//         return (
+//             INDEX
+//         );
+//     }
+// });
+
+// //
+// //
+// // Render
+// //
+// //
+// React.render(<App />, document.getElementById('master'));
